22693f07ef76e623c2d92206da80b6b27cb73375
Edqwird <161136059+Edqwird@users.noreply.github.com>
2025-11-24 21:41:57 +0400

    Add files via upload

--- ekeys.js
+++ ekeys.js
@@ -1,14 +1,29 @@
-const boxFBT = document.querySelectorAll('.front', '.back', '.tools');
+const boxFBT = document.querySelectorAll('.front, .back, .tools');
 const bar = document.querySelectorAll('.tSkill');
+const boxBar = document.querySelector('.blockSkill');
 
-bar.forEach(bart => {
+bar.forEach((bart, index) => {
     bart.addEventListener('click', () => {
-        bar.forEach(item => {
-            item.classList.remove('active');
-            item.classList.remove('invisible');
-        });
+        bar.forEach(item => item.classList.remove('active'));
+
+        boxFBT.forEach(fbt => fbt.style.display = 'none');
         
-        // Затем добавляем классы только к clicked элементу
         bart.classList.add('active');
+
+        if (index === 0) {
+            boxFBT[0].style.display = 'flex';
+            boxBar.style.borderRadius = '0 2rem 2rem 2rem';
+        } else if (index === 1) {
+            boxFBT[1].style.display = 'flex';
+            boxBar.style.borderRadius = '2rem';
+        } else if (index === 2) {
+            boxFBT[2].style.display = 'flex';
+            boxBar.style.borderRadius = '2rem 0 2rem 2rem';
+        }
     })
-})
+}) 
+
+document.addEventListener('DOMContentLoaded', function() {
+    boxFBT.forEach(fbt => fbt.style.display = 'none');
+    boxFBT[0].style.display = 'flex';
+});
